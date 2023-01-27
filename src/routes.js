951a963e0b212790af0e956e0baccac7de1fce15
francis-devbr <francis-devbr@outlook.com>
2023-01-27 10:08:35 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,8 @@
-import Grupo from "./views/Grupo";
 import Index from "./views/Index";
-import Liberacao from "./views/Liberacao";
+
 import MemoriaCalculo from "./views/MemoriaCalculo";
-import MNPEX from "./views/MNPEX";
+
 import NotaFiscal from "./views/NotaFiscal";
-import Relatorios from "./views/Relatorios";
-import Resgate from "./views/Resgate";
 
 var routes = [
   {
@@ -25,56 +22,10 @@ var routes = [
   },
 
   {
-    path: "/resgate",
-    name: "Resgate",
-    icon: "ni ni-satisfied text-primary",
-    element: <Resgate />,
-    layout: "/admin",
-  },
-
-  {
-    path: "/grupo",
-    name: "Grupo",
-    icon: "fa fa-group-arrows-rotate text-primary",
-    element: <Grupo />,
-    layout: "/admin",
-  },
-
-  {
-    path: "/liberacao",
-    name: "Liberacao",
-    icon: "fas fa-list-check text-primary",
-    element: <Liberacao />,
-    layout: "/admin",
-  },
-
-  {
     path: "/memoria-calculo",
     name: "Memoria de Calculo",
     icon: "ni ni-money-coins text-primary",
     element: <MemoriaCalculo />,
-    layout: "/admin",
-  },
-
-  {
-    path: "/mnpex-pe",
-    name: "MNPEX Ponto Extra",
-    icon: "ni ni-single-copy-04 text-primary",
-    element: <MNPEX />,
-    layout: "/admin",
-  },
-  {
-    path: "/relatorios",
-    name: "Relatorios",
-    icon: "ni ni-chart-pie-35 text-primary",
-    element: <Relatorios />,
-    layout: "/admin",
-  },
-  {
-    path: "/david",
-    name: "David",
-    icon: "ni ni-chart-pie-35 text-primary",
-    element: <MNPEX />,
     layout: "/admin",
   },
 ];
