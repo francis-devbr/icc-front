e4335212d9fb3492b56cc391c6ba0f572d5e5f44
francis-devbr <francis-devbr@outlook.com>
2023-01-27 21:43:33 -0300

    .

--- src/layout/PrivateRoute.js
+++ src/layout/PrivateRoute.js
@@ -1,16 +1,11 @@
 import { useKeycloak } from "@react-keycloak/web";
-import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { keycloak } = useKeycloak();
-  const location = useLocation();
+
   const isLoggedIn = keycloak.authenticated;
 
-  return isLoggedIn ? (
-    children
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  return isLoggedIn ? children : null;
 };
 
 export default PrivateRoute;
