2e188c3e7e8a04839e6f32435c38ba7ad4c83922
francis-devbr <francis-devbr@outlook.com>
2023-02-12 17:12:51 -0300

    .

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
   url: "http://34.148.171.98/auth",
   realm: "icc",
-  clientId: "react-icc-client",
+  clientId: "icc-front",
 });
 
 export default keycloak;
