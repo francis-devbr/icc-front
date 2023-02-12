19166287c0c37f85863d31a37f0adeb49a6b44c7
David Santos <dd_david@msn.com>
2023-02-12 19:49:50 -0300

    Tela de Cadastros Gerais

--- src/pages/cadastro-geral/CadastrosGerais.jsx
+++ src/pages/cadastro-geral/CadastrosGerais.jsx
@@ -5,6 +5,7 @@ import {
   Card,
   CardBody,
   CardHeader,
+  CardTitle,
   Col,
   Container,
   Row,
@@ -21,21 +22,145 @@ const CadastrosGerais = () => {
       <HeaderNoInfo />
       <Container className="mt--7" fluid>
         <Row>
-          <Col>
+          <Col lg="6">
             <Card className="shadow">
               <CardHeader className="border-0">
                 {keycloak?.hasResourceRole("manager") && (
-                  <Button
-                    color="success"
-                    className="btn mb-2 "
-                    onClick={() => navigate("/admin/pessoas/new")}
-                  >
-                    <i className="fas fa-clipboard-check"></i> Adicionar
-                  </Button>
+                  <Row>
+                    <Col lg="6" xl="6">
+                      <Card className="card-stats mb-4 mb-xl-0">
+                        <CardBody>
+                          <Row>
+                            <div className="col">
+                              <CardTitle
+                                tag="h5"
+                                className="text-uppercase text-muted mb-0"
+                              >
+                                Lojas
+                              </CardTitle>
+                              <span className="h2 font-weight-bold mb-0">
+                                121
+                              </span>
+                            </div>
+                            <Col className="col-auto">
+                              <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                <i className="fa-solid fa-store" />
+                              </div>
+                            </Col>
+                          </Row>
+                          <Button
+                            color="primary"
+                            className="btn mb-2 w-100 mt-2"
+                            onClick={() => navigate("/admin/ocorrencias/new")}
+                          >
+                            <i className="fa-solid fa-eye"></i> Visualizar
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </Col>
+
+                    <Col lg="6" xl="6">
+                      <Card className="card-stats mb-4 mb-xl-0">
+                        <CardBody>
+                          <Row>
+                            <div className="col">
+                              <CardTitle
+                                tag="h5"
+                                className="text-uppercase text-muted mb-0"
+                              >
+                                Formato Loja
+                              </CardTitle>
+                              <span className="h2 font-weight-bold mb-0">
+                                5
+                              </span>
+                            </div>
+                            <Col className="col-auto">
+                              <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                <i className="fas fa-store-slash" />
+                              </div>
+                            </Col>
+                          </Row>
+                          <Button
+                            color="primary"
+                            className="btn mb-2 w-100 mt-2"
+                            onClick={() => navigate("/admin/ocorrencias/new")}
+                          >
+                            <i className="fa-solid fa-eye"></i> Visualizar
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    
+
+                    <Col lg="6" xl="6" className="mt-3">
+                      <Card className="card-stats mb-4 mb-xl-0">
+                        <CardBody>
+                          <Row>
+                            <div className="col">
+                              <CardTitle
+                                tag="h5"
+                                className="text-uppercase text-muted mb-0"
+                              >
+                                Natureza Fato
+                              </CardTitle>
+                              <span className="h2 font-weight-bold mb-0">
+                                3
+                              </span>
+                            </div>
+                            <Col className="col-auto">
+                              <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                <i className="ni ni-book-bookmark" />
+                              </div>
+                            </Col>
+                          </Row>
+                          <Button
+                            color="primary"
+                            className="btn mb-2 w-100 mt-2"
+                            onClick={() => navigate("/admin/ocorrencias/new")}
+                          >
+                            <i className="fa-solid fa-eye"></i> Visualizar
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </Col>
+
+                    <Col lg="6" xl="6" className="mt-3">
+                      <Card className="card-stats mb-4 mb-xl-0">
+                        <CardBody>
+                          <Row>
+                            <div className="col">
+                              <CardTitle
+                                tag="h5"
+                                className="text-uppercase text-muted mb-0"
+                              >
+                                Pessoas
+                              </CardTitle>
+                              <span className="h2 font-weight-bold mb-0">
+                                354
+                              </span>
+                            </div>
+                            <Col className="col-auto">
+                              <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                                <i className="ni ni-circle-08" />
+                              </div>
+                            </Col>
+                          </Row>
+                          <Button
+                            color="primary"
+                            className="btn mb-2 w-100 mt-2"
+                            onClick={() => navigate("/admin/ocorrencias/new")}
+                          >
+                            <i className="fa-solid fa-eye"></i> Visualizar
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                  
                 )}
               </CardHeader>
               <CardBody>
-                <List />
+                {/* <List /> */}
               </CardBody>
             </Card>
           </Col>
