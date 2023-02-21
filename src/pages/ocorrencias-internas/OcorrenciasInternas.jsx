91463ae87830cf188ccf5515a626558aeee2e235
francis-devbr <francis-devbr@outlook.com>
2023-02-21 17:33:58 -0300

    .

--- src/pages/ocorrencias-internas/OcorrenciasInternas.jsx
+++ src/pages/ocorrencias-internas/OcorrenciasInternas.jsx
@@ -9,46 +9,40 @@ import {
   Col,
   Container,
   Row,
-  Modal, ModalHeader, ModalBody, ModalFooter
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
 } from "reactstrap";
 
+import ReactDOM from "react-dom";
+import { PDFViewer } from "@react-pdf/renderer";
 
-import ReactDOM from 'react-dom';
-import { PDFViewer } from '@react-pdf/renderer';
-
-
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 
 import List from "./List";
 import { useState } from "react";
 
 const OcorrenciasInternas = () => {
-  
   const navigate = useNavigate();
 
   const styles = StyleSheet.create({
     page: {
-      flexDirection: 'row',
-      backgroundColor: '#E4E4E4',
+      flexDirection: "row",
+      backgroundColor: "#E4E4E4",
       width: 100,
     },
     section: {
       margin: 10,
       padding: 10,
-      flexGrow: 1
-    }
+      flexGrow: 1,
+    },
   });
- 
 
+  const MyDocument = () => alert("teste");
 
-  const MyDocument = () => (
-   alert('teste')
-  );
-
-
-  const PDF = () => (
-    ReactDOM.render(<MyDocument />, document.getElementById('root'))
-  );
+  const PDF = () =>
+    ReactDOM.render(<MyDocument />, document.getElementById("root"));
 
   const [modal, setModal] = useState(false);
 
@@ -61,25 +55,6 @@ const OcorrenciasInternas = () => {
         <Row>
           <Col>
             <Card className="shadow" style={{ minHeight: "550px" }}>
-<<<<<<< HEAD
-              <CardHeader className="border-0">
-                {keycloak?.hasResourceRole("manager") && (
-                  <><Button
-                    color="success"
-                    className="btn mb-2 "
-                    onClick={() => navigate("/admin/ocorrencias-internas/new")}
-                  >
-                    <i className="ni ni-briefcase-24"></i> Adicionar Nova
-                    Ocorrência
-                  </Button><Button
-                    color="danger"
-                    className="btn mb-2 "
-                    onClick={toggle}
-                  >
-                      <i className="fas fa-file-pdf"></i> Gerar Relatório PDF
-                    </Button></>
-                )}
-=======
               <CardHeader>
                 <Row className="align-items-center">
                   <Col className="text-left" xs="12">
@@ -94,7 +69,6 @@ const OcorrenciasInternas = () => {
                     </Button>
                   </Col>
                 </Row>
->>>>>>> 529d4c9f4a4113e641e280737bd4391d8c474ca7
               </CardHeader>
               <CardBody>
                 <List />
@@ -104,50 +78,42 @@ const OcorrenciasInternas = () => {
         </Row>
       </Container>
       <div>
-      
-       
-        
-      
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            <Container fluid>
+              <Row>
+                <Col md={12}>
+                  <PDFViewer>
+                    <Document>
+                      <Page size="A4" style={styles.page}>
+                        <View style={styles.section}>
+                          <Text>Case Principal</Text>
+                        </View>
+                        <View style={styles.section}>
+                          <Text>COD LOJA</Text>
+                        </View>
+                        <View style={styles.section}>
+                          <Text>REGIÃO</Text>
+                        </View>
+                      </Page>
+                    </Document>
+                  </PDFViewer>
+                </Col>
+              </Row>
+            </Container>
+          </ModalBody>
 
-        <Modal isOpen={modal} toggle={toggle} >
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-      <Container fluid>
-        <Row> 
-          <Col md={12}>
-            <PDFViewer>
-              <Document>
-                <Page size="A4" style={styles.page}>
-                  <View style={styles.section}>
-                    <Text>Case Principal</Text>
-                  </View>
-                  <View style={styles.section}>
-                    <Text>COD LOJA</Text>
-                  </View>
-                  <View style={styles.section}>
-                    <Text>REGIÃO</Text>
-                  </View>
-                </Page>
-              </Document>
-            </PDFViewer>
-          </Col>
-        </Row>
-      </Container>
-      </ModalBody>
-          
-        
-      <ModalFooter>
-          <Button color="success" onClick={toggle}>
-            Baixar Relatório
-          </Button>{' '}
-          <Button color="danger" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-       </Modal>
-     
-    </div>
-      
+          <ModalFooter>
+            <Button color="success" onClick={toggle}>
+              Baixar Relatório
+            </Button>{" "}
+            <Button color="danger" onClick={toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
     </>
   );
 };
